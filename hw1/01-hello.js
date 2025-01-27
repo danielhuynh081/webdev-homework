7f5d449a59e581814fd3ac70d3032688843e67ce
daniel huynh <danielhuynh@daniels-MacBook-Air.local>
2025-01-26 19:21:25 -0800

    Wave and Lighthouse extension remaining

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,9 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("page loaded successfully");
+
+  console.log(
+    "My name is Daniel and I'm currently taking CS 463/563: Intro to Web Development this quarter. Some of my hobbies are lifting weights, concerts, and eating everything in my way :3 I'm excited to learn more about web development and how to create beautiful websites."
+  );
 }
